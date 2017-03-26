014fa43fe4520011aabee7f5ef5c1d0fda48e1f0
Yuriy Liashkov <yura.lyashkov@yandex.by>
2017-03-27 00:29:30 +0300

    Scrolly

--- app/js/app.js
+++ app/js/app.js
@@ -10,4 +10,27 @@ $(function () {
         return false;
     });
 
+    //MENU
+
+    $('.header__navigation').scroolly([
+        {
+            direction: 1,
+            from: 'con-top + 41el',
+            css: {
+                '-transition': 'all .7s',
+                position: 'fixed',
+                top: '-110px'
+            }
+        },
+        {
+            direction: -1,
+            from: 'con-top',
+            css: {
+                '-transition': 'all .7s',
+                position: 'fixed',
+                top: '41px'
+            }
+        }
+    ], $('body'));
+
 });
