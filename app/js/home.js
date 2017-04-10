5e205f830730d83a076c1fa3c7fa9f6ed59e92c7
Kozemiakina <alisa9a@tut.by>
2017-04-10 11:47:48 +0300

    27.03

--- app/js/home.js
+++ app/js/home.js
@@ -34,8 +34,68 @@ $(function () {
         return false;
     });
 
-});
 
+});
+/**
+ * Drop-down
+ * @param seo
+ */
 function textMsg(seo) {
     $('#seo').slideToggle();
 }
+
+
+/**
+ * Drop-down li in burger menu
+ */
+$(function () {
+    /**
+     * Drop-down in burger menu
+     */
+    $("a.burger__down").click(function () {
+        $('body').css({"overflow":"hidden"});
+        $('.profile__menu').css({"display":"none"});
+        $(this).closest(".burger").find(".burger__menu").slideToggle();
+        return false;
+    });
+    /**
+     * Drop-down li in burger menu
+     */
+    $("a.drop__down").click(function () {
+        $(this).toggleClass("drop__down__active");
+        $(this).closest("li").find(".drop__down__menu").slideToggle();
+        return false;
+
+    });
+    /**
+     * Drop-down li in burger menu
+     */
+    $("a.burger__button").click(function () {
+        $(this).closest(".burger").find(".burger__menu").slideToggle();
+        return false;
+
+    });
+    /**
+     * Drop-down profile menu
+     */
+    $("a.profile__down").click(function () {
+        $('body').css({"overflow":"hidden"});
+        $('.burger__menu').css({"display":"none"});
+        $(this).closest(".profile").find(".profile__menu").slideToggle();
+        return false;
+    });
+
+    /**
+     * Change position of block
+     */
+    var min_width = 1024;
+    $(window).on('resize', function() {
+        var new_width = $(window).width();
+        var container = $('#container');
+        if (new_width <= min_width && !container.hasClass('compact')) {
+            container.addClass('compact');
+            $('#third').insertBefore($('#first'));
+
+        }
+    }).trigger('resize');
+});
