6f9c62a5a916bbb1aa0bfe2559a6300a6f1c264b
Kate-Kiryanova <kate.kiryanovaaa@gmail.com>
2017-04-22 15:26:26 +0300

    22.04 Kate

--- app/js/new-calculator.js
+++ app/js/new-calculator.js
@@ -16,7 +16,30 @@ $( function() {
     });
     $( "#amount" ).val( "руб." + $( "#apartmen-cost" ).slider( "value" ) );
 
+    $( "#apartmen-cost-mob" ).slider({
+        range: "min",
+        value: 6.7,
+        min: 1,
+        max: 15,
+        step: 0.1,
+        animate: true,
+        slide: function( event, ui ) {
+            $( "#amount-mob" ).val( "руб." + ui.value );
+        }
+    });
+    $( "#amount-mob" ).val( "руб." + $( "#apartmen-cost-mob" ).slider( "value" ) );
+
     $( "#year" ).slider({
+        range: "min",
+        value: 2,
+        min: 1,
+        max: 11,
+        step: 1,
+        orientation: "horizontal",
+        animate: true
+    });
+
+    $( "#year-mob" ).slider({
         range: "min",
         value: 2,
         min: 1,
@@ -36,7 +59,27 @@ $( function() {
         animate: true
     });
 
+    $( "#month-mob" ).slider({
+        value: 10,
+        min: 1,
+        max: 12,
+        step: 1,
+        orientation: "horizontal",
+        range: "min",
+        animate: true
+    });
+
     $( "#age" ).slider({
+        value: 30,
+        min: 25,
+        max: 45,
+        step: 1,
+        orientation: "horizontal",
+        range: "min",
+        animate: true
+    });
+
+    $( "#age-mob" ).slider({
         value: 30,
         min: 25,
         max: 45,
